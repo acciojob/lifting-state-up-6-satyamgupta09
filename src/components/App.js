1955a4ff09f9b205c8162771570a1bd568157e9a
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-01-24 03:36:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
+import TodoList from "./TodoList";
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    { task: "Learn React", status: false },
+    { task: "Build a React app", status: false },
+    { task: "Deploy the React app", status: false },
+  ]);
+
+  function handleComplete(index) {
+    // console.log("hitting", index);
+    setTodos((prevTodos) => {
+      const updateTodos = [...prevTodos];
+      updateTodos[index].status = true;
+      return updateTodos;
+    });
+    // console.log(todos[index].status);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Parent Component</h1>
+      <TodoList todos={todos} handleComplete={handleComplete} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
