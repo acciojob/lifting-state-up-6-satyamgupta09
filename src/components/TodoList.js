4f5860abeac8de027dfde573b8603f3b42819adc
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-01-24 03:38:10 +0530

    Create TodoList.js

---
+++ src/components/TodoList.js
@@ -0,0 +1,25 @@
+export default function TodoList({ todos, handleComplete }) {
+  return (
+    <div>
+      <h2>Child Component</h2>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo.task}
+            {todo.status ? (
+              ""
+            ) : (
+              <button
+                onClick={() => {
+                  handleComplete(index);
+                }}
+              >
+                Complete
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
