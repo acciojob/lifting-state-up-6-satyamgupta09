c41781dbbb8fab62942414e4a8131ea6d44aa8a3
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-01-26 02:53:26 +0530

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,28 @@
+// import React from "react";
+// export default function TodoList({ todos, handleComplete }) {
+//   return (
+//     <div>
+//       <h2>Child Component</h2>
+//       <ul>
+//         {todos.map((todo, index) => (
+//           <li key={todo.id}>
+//             {todo.task}
+//             {!todo.completed && (
+//               <button onClick={() => handleComplete(todo.id)}>Complete</button>
+//             )}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
 import React from "react";
 export default function TodoList({ todos, handleComplete }) {
   return (
     <div>
       <h2>Child Component</h2>
       <ul>
-        {todos.map((todo, index) => (
+        {todos.map((todo) => (
           <li key={todo.id}>
             {todo.task}
             {!todo.completed && (
